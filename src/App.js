dea8ab84a5f11eaf152525b67d6a40c98dd25dcd
amin roosta <amin.roosta@outlook.com>
2017-01-10 23:48:44 +0330

    add color-pikcker & slider components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { SketchPicker } from 'react-color'
+import Slider from 'rc-slider'
+
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world!</h1>
-    );
+      <div>
+        <h1>Hello, world!</h1>
+        <Slider tipTransitionName="rc-slider-tooltip-zoom-down" onChange={value => this.log(value)} />
+      </div>
+    )
+        //<SketchPicker />
+  }
+  log(value) {
+    console.warn(value)
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+require('!style!css!../node_modules/rc-slider/assets/index.css')
+
 ReactDOM.render(<App />, document.getElementById('root'));
