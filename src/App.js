2c639126679cffdef8c62389fd78cbd68d57092d
amin roosta <amin.roosta@outlook.com>
2017-01-11 17:33:00 +0330

    add slider component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import BorderComponent from './components/border-component'
 
 import borderStore from './stores/border-store'
 import TabbarComponent from './components/tabbar-component'
+import SliderComponent from './components/slider-component'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <TabbarComponent />
+        <SliderComponent />
+        <SliderComponent />
         <BorderComponent store={borderStore} />
       </div>
     )
---
+++ src/components/slider-component.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import {observer} from 'mobx-react'
+import {colors, sizes} from '../constants/theme'
+import Slider from 'rc-slider'
+
+@observer
+export default class SliderComponent extends Component {
+    render() {
+        const {store} = this.props;
+        return (
+            <div style={styles.container}>
+                <div style={styles.subcontainer}>
+                    <div style={styles.title}>Title: </div>
+                    <Slider defaultValue={50} min={0} max={100}
+                            onChange={value => console.warn(value) } />
+                </div>
+                <div style={styles.underline} />
+            </div>
+        )
+    }
+}
+
+const height = 50;
+const styles = {
+    container: {
+        background: colors.white,
+        height: height,
+        display: 'flex',
+        flexFlow: 'row wrap',
+        padding: '7px 10px 0 10px',
+        boxSizing: 'border-box',
+    },
+    subcontainer: {
+        width: '100%',
+        display: 'flex',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        width: '30%',
+        fontSize: sizes.medium
+    },
+    slider: {
+        width: '60%',
+    },
+    underline: {
+        borderBottom: `1px solid ${colors.dark}`,
+        alignSelf: 'flex-end',
+        width: '100%'
+    }
+}
